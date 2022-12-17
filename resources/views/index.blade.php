2091c05b7bd3744a09e7464b7d3c54ec70facf68
qasim-dzinemedia <qasim@dzinemedia.com>
2022-12-17 14:01:54 +0500

    changes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,60 +1,62 @@
 @extends('layouts.app')
 @section('content')
-<section class="fade-blue">
-<div class="container">
-    <nav class="navbar navbar-expand-lg">
-            <a class="navbar-brand" href="#"><img src="assets/logo.svg" alt=""></a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                <li class="nav-item me-50">
-                    <a class="nav-link active" aria-current="page" href="#">HOME</a>
-                </li>
-
-                <li class="nav-item dropdown me-50">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                        data-bs-toggle="dropdown" aria-expanded="false">
-                        SERVICE
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li>
-                            <hr class="dropdown-divider">
+    <section class="fade-blue">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg">
+                <a class="navbar-brand" href="#"><img src="assets/logo.svg" alt=""></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item me-50">
+                            <a class="nav-link active" aria-current="page" href="#">HOME</a>
                         </li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                </li>
-                <li class="nav-item dropdown me-50">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                        data-bs-toggle="dropdown" aria-expanded="false">
-                        ABOUT US
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                </li>
-                <li class="nav-item me-2">
-                    <a class="nav-link secondary-bg text-white btn-md" href="#"> <i><img
-                                src="assets/icons/login.svg" alt="ordernow"></i> LOGIN</a>
-                </li>
 
-                <li class="nav-item">
-                    <a class="nav-link primary_bg text-white btn-md" href="#"> <i><img
-                                src="assets/icons/ordernow.svg" alt="ordernow"></i> ORDER NOW</a>
-                </li>
-            </ul>
+                        <li class="nav-item dropdown me-50">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                                SERVICE
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown me-50">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                                ABOUT US
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item me-2">
+                            <a class="nav-link secondary-bg text-white btn-md" href="#"> <i><img
+                                        src="assets/icons/login.svg" alt="ordernow"></i> LOGIN</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link primary_bg text-white btn-md" href="#"> <i><img
+                                        src="assets/icons/ordernow.svg" alt="ordernow"></i> ORDER NOW</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         </div>
-      </nav>
-</div>
-</section>
+    </section>
 
 
 
@@ -69,8 +71,12 @@
                     <h1 class="heading">
                         Professional Academic&nbsp;<span class="text_secondary d-inline-block">Writing</span> Service
                     </h1>
-                    <p class="my-3">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut </p>
-                    <a class="btn primary_bg text-white btn-md hover_btn" href=""> <img src="assets/icons/Quill.svg" alt="Quill"> Write My Paper</a>
+                    <div class="my-3 text-lg-start text-center">
+                        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt
+                            ut </p>
+                        <a class="btn mt-3 primary_bg text-white btn-md hover_btn" href=""> <img
+                                src="assets/icons/Quill.svg" alt="Quill"> Write My Paper</a>
+                    </div>
                 </div>
 
                 <div class="col-lg-6">
