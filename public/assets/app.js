d6f7ab7bdf44450134f7ca4a426d6c37e18995a5
qasim-dzinemedia <qasim@dzinemedia.com>
2022-12-21 17:29:37 +0500

    home page
    testimonial slider completed

--- public/assets/app.js
+++ public/assets/app.js
@@ -3,3 +3,26 @@ function rotate() {
     let nav_arrows = document.getElementsByClassName("nav_arrow");
     nav_arrows.classList.toggle('arrows_rotation');
 }
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    margin: 80,
+    loop: true,
+    nav: true,
+    // autoplay: true,
+    lazyLoad: true,
+
+    navText: ["<img src='../assets/icons/nextslide.svg'>", "<img src='../assets/icons/prevslide.svg'>"],
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            center: true,
+            items: 3
+        }
+    }
+});
